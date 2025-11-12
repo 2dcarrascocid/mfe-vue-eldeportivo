caaab6e43e6573256a39b8925252bce68d584704
dcarrasco <dcarrasco@servel.cl>
2025-11-12 16:42:58 -0300

    nuevo proyect

---
+++ shell/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-blue-600 text-white">
+    <h1 class="text-4xl font-bold">✅ Tailwind funcionando en shell Usuarios</h1>
+  </div>
+</template>
+  
+  <script setup>
+  // Nada aún
+  </script>
+  
+  <style>
+  body {
+    font-family: system-ui, sans-serif;
+    margin: 2rem;
+  }
+  </style>
+  
